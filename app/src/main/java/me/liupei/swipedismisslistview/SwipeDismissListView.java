9106a60d97e79f9ad4575be12d86cc9a98d8a990
刘沛 <liupei02@meituan.com>
2015-09-01 19:38:14 +0800

    fix bugs & fit headers

--- app/src/main/java/me/liupei/swipedismisslistview/SwipeDismissListView.java
+++ app/src/main/java/me/liupei/swipedismisslistview/SwipeDismissListView.java
@@ -1,5 +1,7 @@
 package me.liupei.swipedismisslistview;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by liupei on 15/8/14.
@@ -65,48 +68,55 @@ public class SwipeDismissListView extends ListView {
 
                 if (Math.abs(x - downX) < getWidth() * DISMISS_FACTOR) {
                     //还原
-                    currentView.startAnimation(new XTranslationAnim(currentView, 0, null));
-
+                    xTranslationAnim(currentView, 0, null);
                 } else {
                     //dismiss
                     if (x - downX >= 0) {
                         //向右
-                        currentView.startAnimation(new XTranslationAnim(currentView, getWidth(), new Animation.AnimationListener() {
+                        xTranslationAnim(currentView, getWidth(), new Animator.AnimatorListener() {
                             @Override
-                            public void onAnimationStart(Animation animation) {
+                            public void onAnimationStart(Animator animation) {
 
                             }
 
                             @Override
-                            public void onAnimationEnd(Animation animation) {
-                                shorten(currentView, currentIndex);
+                            public void onAnimationEnd(Animator animation) {
+                                shorten(currentView, currentIndex + getFirstVisiblePosition() - getHeaderViewsCount());
                             }
 
                             @Override
-                            public void onAnimationRepeat(Animation animation) {
+                            public void onAnimationCancel(Animator animation) {
 
                             }
-                        }));
 
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+
+                            }
+                        });
                     } else {
                         //向左
-                        currentView.startAnimation(new XTranslationAnim(currentView, -getWidth(), new Animation.AnimationListener() {
+                        xTranslationAnim(currentView, -getWidth(), new Animator.AnimatorListener() {
                             @Override
-                            public void onAnimationStart(Animation animation) {
+                            public void onAnimationStart(Animator animation) {
 
                             }
 
                             @Override
-                            public void onAnimationEnd(Animation animation) {
-                                shorten(currentView, currentIndex);
+                            public void onAnimationEnd(Animator animation) {
+                                shorten(currentView, currentIndex + getFirstVisiblePosition() - getHeaderViewsCount());
                             }
 
                             @Override
-                            public void onAnimationRepeat(Animation animation) {
+                            public void onAnimationCancel(Animator animation) {
 
                             }
-                        }));
 
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+
+                            }
+                        });
                     }
                 }
             }
@@ -138,11 +148,13 @@ public class SwipeDismissListView extends ListView {
 
                 handleCurrentIndex();
 
+                Toast.makeText(getContext(), "onDown:" + currentIndex + " onFirst:" + getFirstVisiblePosition(), Toast.LENGTH_SHORT).show();
+
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
 
-                //判断点
+                //判断点还是滑动
                 if (Math.abs(x - downX) > HORIZONTAL_SCROLL_THRESHOLD
                         && Math.abs(x - downX) > Math.abs(y - downY) * HORIZONTAL_SCROLL_FACTOR) {
                     //水平滑动
@@ -179,25 +191,12 @@ public class SwipeDismissListView extends ListView {
 
     }
 
-    private class XTranslationAnim extends Animation {
-
-        private View v;
-        private float src, desc;
-
-        public XTranslationAnim(View v, float desc, AnimationListener listener) {
-            setAnimationListener(listener);
-            this.v = v;
-            this.desc = desc;
-            this.src = v.getTranslationX();
-            this.setDuration(ANIM_DURATION);
-        }
-
-        @Override
-        protected void applyTransformation(float interpolatedTime, Transformation t) {
-
-            v.setTranslationX(src + (desc - src) * interpolatedTime);
-
-        }
+    private void xTranslationAnim(View v, float desc, Animator.AnimatorListener listener) {
+        ObjectAnimator anim = ObjectAnimator.ofFloat(v, "translationX", v.getTranslationX(), desc);
+        anim.setDuration(ANIM_DURATION);
+        if (listener != null)
+            anim.addListener(listener);
+        anim.start();
     }
 
     private class HeightAnim extends Animation {
@@ -205,7 +204,7 @@ public class SwipeDismissListView extends ListView {
         private View v;
         private int src, desc;
 
-        public HeightAnim(View v, int desc, AnimationListener listener){
+        public HeightAnim(View v, int desc, AnimationListener listener) {
             setAnimationListener(listener);
             this.v = v;
             this.desc = desc;
@@ -216,13 +215,16 @@ public class SwipeDismissListView extends ListView {
         @Override
         protected void applyTransformation(float interpolatedTime, Transformation t) {
 
+            if (hasEnded())
+                return;
             v.getLayoutParams().height = (int) (src + (desc - src) * interpolatedTime);
             v.requestLayout();
 
         }
     }
 
-    private void shorten(final View v, final int index){
+    private void shorten(final View v, final int position) {
+
         v.startAnimation(new HeightAnim(v, 1, new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -232,11 +234,11 @@ public class SwipeDismissListView extends ListView {
             @Override
             public void onAnimationEnd(Animation animation) {
                 //恢复
-                v.getLayoutParams().height = 0;
+                v.getLayoutParams().height = -2;
                 v.requestLayout();
                 v.setTranslationX(0);
-                if(listener != null){
-                    listener.onDismiss(index + getFirstVisiblePosition());
+                if (listener != null) {
+                    listener.onDismiss(position);
                 }
             }
 
@@ -245,13 +247,14 @@ public class SwipeDismissListView extends ListView {
 
             }
         }));
+
     }
 
-    public void setOnDismissListener(OnDismissListener listener){
+    public void setOnDismissListener(OnDismissListener listener) {
         this.listener = listener;
     }
 
-    public interface OnDismissListener{
+    public interface OnDismissListener {
 
         public void onDismiss(int position);
 
